8fe80b18fa2103fc00d75089b8e959a5571070c7
Ricardo Gambirasio <rgfibe@gmail.com>
2013-07-08 01:29:28 -0300

    web.js reads in index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var indexBuff = fs.readFileSync(index.html);
+    var indexBuff = fs.readFileSync('./index.html');
     var indexStr = indexBuff.toString();
   response.send(indexStr);
 });
